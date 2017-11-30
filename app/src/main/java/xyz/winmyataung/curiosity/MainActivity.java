b3bf3a7e7074ba5f90f96d7be15b6317555b89c1
DESKTOP-49KO4JJ\DELL <winmyataung.page15@gmail.com>
2017-12-01 01:08:20 +0630

    curiosity home screen implementation

--- app/src/main/java/xyz/winmyataung/curiosity/MainActivity.java
+++ app/src/main/java/xyz/winmyataung/curiosity/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+     /*   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
